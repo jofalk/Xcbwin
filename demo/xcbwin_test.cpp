912b000f6e11f994d443dbb95a9b9fb62dd91f07
jofalk <johannes.falk@physik.uni-wuerzburg.de>
2013-12-10 16:50:00 +0100

    initial commit of xcbwin-files

---
+++ demo/xcbwin_test.cpp
@@ -0,0 +1,28 @@
+// Example for the use of the Xcbwin class
+
+// must be compiled using the -lxcb flag!
+
+
+
+
+#include "../xcbwin.h"
+#include <cmath>
+
+int main (void)
+{
+Xcbwin W;
+W.Open (800,600);
+
+for (int x=0; x<800; ++x)
+	for (int y=0; y<600; ++y)
+		{
+		W.SetColor((x/300.0+sin(y/100.0)));
+		
+		W.DrawPoint(x,y);
+		
+		}
+W.Wait();
+W.Close();
+}
+
+
